bd98b2478f7b209e4cb7b73d1820e2f3f58c233c
Raul <rulanxa@gmail.com>
2023-11-27 22:22:27 +0100

    conexion creada

--- conexion.php
+++ conexion.php
@@ -1,19 +1,30 @@
-<?php 
+<?php
 
-	function crearConexion() {
-		// Cambiar en el caso en que se monte la base de datos en otro lugar
-		$host = "localhost";
-		$user = "root";
-		$pass = "";
-		$baseDatos = "pac3_daw";
+function crearConexion()
+{
+	// Cambiar en el caso en que se monte la base de datos en otro lugar
+	$host = "sql8.freemysqlhosting.net";
+	$user = "sql8665610";
+	$pass = "jNXB145Ayc";
+	$baseDatos = "sql8665610";
 
-		// Completar...
+	// Crear una conexión
+	$conexion = new mysqli($host, $user, $pass, $baseDatos);
+
+	// Verificar la conexión
+	if ($conexion->connect_error) {
+		die("Error de conexión: " . $conexion->connect_error);
 	}
 
+	// Establecer el juego de caracteres a utf8 (opcional)
+	$conexion->set_charset("utf8");
 
-	function cerrarConexion($conexion) {
-		// Completar...
-	}
+	// Devolver la conexión
+	return $conexion;
+}
 
 
-?>
+function cerrarConexion($conexion)
+{
+	$conexion->close();
+}
