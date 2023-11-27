e0d52786f8aa3351f23ca92f8a5c8862a29f37ae
Raul <rulanxa@gmail.com>
2023-11-27 22:44:58 +0100

    update index

--- index.php
+++ index.php
@@ -10,6 +10,31 @@
 	<?php
 	
 		include "consultas.php";
+		// Verificar si se ha enviado el formulario
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			// Obtener el nombre de usuario y correo electrónico del formulario
+			$nombre = $_POST["nombre"];
+			$correo = $_POST["correo"];
+	
+			// Realizar la consulta para determinar el tipo de usuario
+			$tipoUsuario = tipoUsuario($nombre, $correo);
+	
+			// Almacenar el tipo de usuario en una cookie
+			setcookie("tipo_usuario", $tipoUsuario, time() + 3600, "/"); // Cookie válida por 1 hora
+	
+			// Redirigir a la página correspondiente según el tipo de usuario
+			if ($tipoUsuario == "superadmin") {
+				header("Location: usuarios.php");
+				exit();
+			} elseif ($tipoUsuario == "autorizado") {
+				header("Location: articulos.php");
+				exit();
+			} elseif ($tipoUsuario == "registrado") {
+				echo "Hola $nombre, no tienes permisos para acceder.";
+			} else {
+				echo "El usuario no está registrado.";
+			}
+		}
 
 	?>
 	
