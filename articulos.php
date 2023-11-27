db01713dadf1450d99b18cdc53efb02db8d2d3c7
Raul <rulanxa@gmail.com>
2023-11-27 22:46:17 +0100

    added articulos page

--- articulos.php
+++ articulos.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Articulos</title>
 </head>
+
 <body>
 
-	<?php 
+	<?php
 
-		include "funciones.php";
+	include "funciones.php";
+
+	// Comprobar si el usuario tiene los permisos suficientes
+	$tipoUsuario = isset($_COOKIE['tipo_usuario']) ? $_COOKIE['tipo_usuario'] : '';
+	if ($tipoUsuario != 'autorizado' && $tipoUsuario != 'superadmin') {
+		echo "No tienes permisos para acceder a esta página.";
+		exit();
+	}
+
+	// Obtener el orden de la tabla desde la URL (si está presente)
+	$orden = isset($_GET['orden']) ? $_GET['orden'] : 'ProductID';
+
+	// Obtener la lista de productos
+	$productos = getProductos($orden);
 
 	?>
 
 	<h1>Lista de artículos</h1>
 
+	<?php pintaProductos($orden) ?>
+	<?php
+	// Mostrar enlaces adicionales si el usuario tiene los permisos de la aplicación activados
+	if ($tipoUsuario == 'superadmin') {
+		echo "<p><a href='formArticulo.php?action=anadir'>Añadir nuevo producto</a></p>";
+	}
+
+	// Enlace para volver a index.php
+	echo "<p><a href='index.php'>Volver a Inicio</a></p>";
+	?>
+
 </body>
+
 </html>
